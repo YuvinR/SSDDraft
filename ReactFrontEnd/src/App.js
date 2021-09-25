5f9a80de354db589663476e47005bb2b165bb48f
IT18154368-WickramanayakeT.R.D <it18154368@my.sliit.lk>
2021-09-25 18:43:52 +0530

    clear

--- ReactFrontEnd/src/App.js
+++ ReactFrontEnd/src/App.js
@@ -8,8 +8,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { useAuth0  } from "@auth0/auth0-react";
-
+import { useAuth0 } from "@auth0/auth0-react";
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,54 +24,51 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-
-
-
 function App() {
   const classes = useStyles();
-  const { user, isAuthenticated, getAccessTokenSilently,loginWithRedirect,getIdTokenClaims } = useAuth0();
+  const { user, isAuthenticated, getAccessTokenSilently, loginWithRedirect, getIdTokenClaims } = useAuth0();
 
-  async function  Login(){
+  async function Login() {
     loginWithRedirect();
-    console.log("isAuthenticated",isAuthenticated);
-    
+    console.log("isAuthenticated", isAuthenticated);
+
   }
 
-  async function  Check(){
-    console.log("isAuthenticated",isAuthenticated);
-  
-    const domain = "dev-upkur7si.us.auth0.com";
-    const accessToken = await  getAccessTokenSilently();
-  //  const accessToken = await getAccessTokenSilently({
-  //   audience: `http://localhost:5020`,
-  //   scope: "read:current_user",
-  // });
+  async function Check() {
+    console.log("isAuthenticated", isAuthenticated);
 
-    console.log("accessToken",  accessToken);
+    const domain = "dev-upkur7si.us.auth0.com";
+    const accessToken = await getAccessTokenSilently();
+    //  const accessToken = await getAccessTokenSilently({
+    //   audience: `http://localhost:5020`,
+    //   scope: "read:current_user",
+    // });
+
+    console.log("accessToken", accessToken);
     console.log("getIdTokenClaims", await (await getIdTokenClaims()).__raw);
   }
 
 
   const callSecureApi = async () => {
-   
-      const token = await getAccessTokenSilently();
 
-      const response = await fetch(
-        `http://localhost:5020/WeatherForecast/GetStringTest`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            ContentType: 'application/json',
-            Accept: 'application/json'
-          },
-        }
-      );
+    const token = await getAccessTokenSilently();
 
-      const responseData = await response.json();
-        console.log("data",responseData)
-     
-   
-  }; 
+    const response = await fetch(
+      `http://localhost:5020/WeatherForecast/GetStringTest`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          ContentType: 'application/json',
+          Accept: 'application/json'
+        },
+      }
+    );
+
+    const responseData = await response.json();
+    console.log("data", responseData)
+
+
+  };
 
   return (
     <div className={classes.root}>
@@ -83,8 +80,8 @@ function App() {
           <Typography variant="h6" className={classes.title}>
             News
           </Typography>
-          <Button onClick={()=>Login()} color="inherit">Login</Button>
-          <Button onClick={()=>Check()} color="inherit">Check</Button>
+          <Button onClick={() => Login()} color="inherit">Login</Button>
+          <Button onClick={() => Check()} color="inherit">Check</Button>
           <Button onClick={callSecureApi} color="inherit">Call API</Button>
         </Toolbar>
       </AppBar>
