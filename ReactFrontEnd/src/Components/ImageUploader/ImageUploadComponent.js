3fb36215790afcdf1cc42d0a86bca39bdcefb63b
IT18154368-WickramanayakeT.R.D <it18154368@my.sliit.lk>
2021-09-25 19:32:26 +0530

    .

--- ReactFrontEnd/src/Components/ImageUploader/ImageUploadComponent.js
+++ ReactFrontEnd/src/Components/ImageUploader/ImageUploadComponent.js
@@ -4,11 +4,29 @@ import {
     Card,
     CardContent
 } from '@material-ui/core';
+import ImageUploader from 'react-images-upload';
+
 
 export function ImageUploadComponent({ test }) {
+    const [ImageObject, setImageObject] = useState();
+
+    function onDrop(picture) {
+        console.log("pri ", picture)
+        setImageObject(picture);
+        console.log("setImageObject ", ImageObject)
+    }
+
     return (
         <div>
-            img {test}
+            <ImageUploader
+                withIcon={true}
+                singleImage={true}
+                withPreview={true}
+                buttonText='Upload Your Image'
+                onChange={onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
         </div>
     )
 }
