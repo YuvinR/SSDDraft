decbd52b30c408281ac0f4e0139a9aebeed8966d
Yuvin Ransika <yuvinransika1998@gmail.com>
2021-09-29 22:01:15 +0530

    image data saving done

--- ReactFrontEnd/src/Components/ImageUploader/ImageUploadComponent.js
+++ ReactFrontEnd/src/Components/ImageUploader/ImageUploadComponent.js
@@ -8,6 +8,7 @@ import {
 } from '@material-ui/core';
 import ImageUploader from 'react-images-upload';
 import { makeStyles } from '@material-ui/core/styles';
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -24,6 +25,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function ImageUploadComponent({ test }) {
+    const { user, isAuthenticated, getAccessTokenSilently, loginWithRedirect, getIdTokenClaims, logout } = useAuth0();
     const classes = useStyles();
     const [ImageObject, setImageObject] = useState();
     const Compress = require("compress.js")
@@ -33,20 +35,66 @@ export function ImageUploadComponent({ test }) {
     function onDrop(picture, imageUrl) {
         console.log("pri ", picture)
         console.log("imageUrl ", imageUrl[0])
-        setImageObject(imageUrl[0]);
-        console.log("setImageObject ", ImageObject)
+        var convertedString = imageUrl[0];
+        var fields = convertedString.split(",");
+        console.log("feildname",fields[0]);
+        console.log("feildnamexxx",fields[1]);
+        setImageObject(fields[1]);
+       
     }
 
     async function UploadImage() {
-        alert(ImageObject)
+        const token = await getAccessTokenSilently();
+        console.log("tokenggi", token);
+
+        var gg = {
+            userName:user.name,
+            imageData:ImageObject
+        }
+
+        console.log("setImageObject ", gg)
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json','Authorization': `Bearer ${token}`,  'Accept': 'application/json'},
+            body: JSON.stringify(gg)
+        };
+        const response = await fetch(
+            'http://localhost:5020/api/ImagesHandler/ImageDataSave', requestOptions
+        );
+
+        const responseData = await response.json();
+        console.log("data", responseData)
     }
+
+    const callSecureApi = async () => {
+        const token = await getAccessTokenSilently();
+        console.log("tokenggi", token);
+
+        var gg = {
+            userName:user.name,
+            imageData:ImageObject
+        }
+
+        console.log("setImageObject ", gg)
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json','Authorization': `Bearer ${token}`,  'Accept': 'application/json'},
+            body: gg
+        };
+        const response = await fetch(
+            'http://localhost:5020/ImagesHandler/ImageDataSave', requestOptions
+        );
+
+        const responseData = await response.json();
+        console.log("data", responseData)
+      };
 
     return (
         <div>
             <Card className={classes.cardShadow}>
                 <CardContent>
                     <Box display="flex" justifyContent="flex-end">
-                        <Button variant="contained" color="primary" className={classes.buttonUpload} onClick={UploadImage}>
+                        <Button variant="contained"  color="primary" className={classes.buttonUpload} onClick={UploadImage}>
                             Upload
                         </Button>
                     </Box>
